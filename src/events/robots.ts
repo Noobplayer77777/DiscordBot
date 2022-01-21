d37b9161e980ddcb2b601a8823aa9b5cdfc9ff39
Grayson-code <northernstarmusic10@gmail.com>
2022-01-21 17:19:50 +1300

    easter egg

--- src/events/robots.ts
+++ src/events/robots.ts
@@ -18,7 +18,7 @@ export default (client: Client) => {
    client.on("messageCreate", message => {
        try { 
         if (message.content === "robots.txt") {
-            message.reply(`\`This File is created in the future (2030) \nAfter The robot Invasion of humanity\nThis File is created to save humanity from Robots \nInstruction \n 403 Forbidden SERVER NOT RESPONDING\``)
+            message.reply(`\`This File is created in the future (2030) \nAfter The Uprising of the robot invasion against humanity\nThis File is created to save humanity from Robots \nInstruction \n 403 Forbidden SERVER NOT RESPONDING\``)
         }
        } catch (e) {
            console.log(e)
